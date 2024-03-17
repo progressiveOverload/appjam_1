188469ba1dc9e76d69e8465576c42378599da697
cihan <cihanisikal@outlook.com>
2024-03-18 02:06:40 +0300

    removed turning back

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -183,6 +183,7 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('My Current Map'),
       ),
       body: Column(
